e07b3dfc5cd43f0cab09bf34266086523d1c1fbc
rizrmdhn <114452320+rizrmdhn@users.noreply.github.com>
2025-10-21 12:40:11 +0800

    feat: add certifications and experiences tables with relationships in the database schema

--- src/server/db/relations.ts
+++ src/server/db/relations.ts
@@ -1,9 +1,16 @@
 import { relations } from "drizzle-orm";
-import { sessions, users } from "./schema";
+import { projects, projectViews, sessions, users } from "./schema";
 
 export const sessionRelations = relations(sessions, ({ one }) => ({
 	users: one(users, {
 		fields: [sessions.userId],
 		references: [users.id],
+	}),
+}));
+
+export const projectRelations = relations(projects, ({ one }) => ({
+	projectView: one(projectViews, {
+		fields: [projects.id],
+		references: [projectViews.projectId],
 	}),
 }));
